d7be7c1338d9e1a97aedb5b0c991f307ba949be5
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2019-12-26 15:22:11 +0900

    coverails実装

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@
 # it.
 #
 require 'simplecov'
+require 'coveralls'
+Coveralls.wear!
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   ENV['RAILS_ENV'] = 'test'
