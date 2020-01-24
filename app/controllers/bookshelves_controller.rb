8da3702c41be77bd88a413fea673f36cc37af789
kazuyoshi-tech <55447682+kazuyoshi-tech@users.noreply.github.com>
2020-01-24 23:38:30 +0900

    Update bookshelves_controller.rb
    インデント 修正

--- app/controllers/bookshelves_controller.rb
+++ app/controllers/bookshelves_controller.rb
@@ -3,13 +3,11 @@ class BookshelvesController < ApplicationController
 
 	def create
 	  book = Book.find_or_initialize_by(book_code: params[:book_code])
-
 	  if book.new_record?
-        results = RakutenWebService::Books::Book.search(isbn: params[:book_code])
+          results = RakutenWebService::Books::Book.search(isbn: params[:book_code])
 	    book = Book.new(read(results.first))
 	    book.save
 	  end
-
 	  bookshelf = Bookshelf.new
 	  bookshelf.user_id = current_user.id
 	  bookshelf.book_id = book.id
@@ -22,19 +20,19 @@ class BookshelvesController < ApplicationController
 	  bookshelf = current_user.bookshelves.find(params[:id])
 	  if bookshelf.update(bookshelf_update_params)
 	    flash[:success_update]  = "「#{bookshelf.book.title}」のステータスを更新しました"
-		redirect_to user_path(current_user.id)
+	    redirect_to user_path(current_user.id)
 	  end
 	end
 
 	def destroy
 	  bookshelf = current_user.bookshelves.find(params[:id])
-      bookshelf.destroy
-      redirect_to user_path(current_user.id)
+          bookshelf.destroy
+          redirect_to user_path(current_user.id)
 	end
 
 	private
 
-    def bookshelf_update_params
+        def bookshelf_update_params
 	  params.require(:bookshelf).permit(:status)
 	end
 
