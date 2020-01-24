50f047542ecae935cb8015b86821e505d15cd994
kazuyoshi-tech <55447682+kazuyoshi-tech@users.noreply.github.com>
2020-01-24 23:57:44 +0900

    インデント 修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-	before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-	  def after_sign_in_path_for(resource)
+    def after_sign_in_path_for(resource)
       user_path(current_user.id)
     end
 
@@ -13,14 +13,12 @@ class ApplicationController < ActionController::Base
 
     def search_items(title)
       books = []
-
       results = RakutenWebService::Books::Book.search({
         title: title,
         booksGenreId: '001005',
         sort: 'sales',
         hits: '24',
       })
-
       results.each do |result|
         book = Book.new(read(result))
         books << book
